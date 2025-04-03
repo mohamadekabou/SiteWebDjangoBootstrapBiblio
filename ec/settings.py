2a85ce4699dc5dae762ee4c5210df413d9a06874
mohamadekabou <164761289+mohamadekabou@users.noreply.github.com>
2025-04-03 17:01:08 +0200

    Update settings.py

--- ec/settings.py
+++ ec/settings.py
@@ -23,7 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!+q)i15)3-50b%m8wt=$o8neik&*y+=d!g=d9)f8wc%i0x+9q8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
+# Production settings
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 ALLOWED_HOSTS = ['*']
 
@@ -52,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ec.urls'
